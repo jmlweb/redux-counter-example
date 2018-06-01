9536117c79af7cb4a9bbad5105cd7c10d191c7d1
José Manuel Lucas <josemanuel@jmlweb.es>
2018-06-01 22:26:34 +0200

    Counter

--- src/App/index.js
+++ src/App/index.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 
 import View from './view';
 
-const App = ({ counter }) => <View counter={0} />;
+const App = ({ counter }) => <View counter={counter} />;
 
 const mapStateToProps = state => ({
   counter: state.counter.current,
