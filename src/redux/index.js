6d17ca129a61465975d87b1faae376bee13f8dc7
José Manuel Lucas <josemanuel@jmlweb.es>
2018-06-02 00:00:44 +0200

    thunk config

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,4 +1,5 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
 
@@ -6,6 +7,7 @@ import rootReducer from './reducers';
 const store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk),
 );
 /* eslint-enable */
 
