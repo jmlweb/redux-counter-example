aad17e4fe0145773f3171ca61400b19f798b7516
José Manuel Lucas <josemanuel@jmlweb.es>
2018-06-01 23:52:32 +0200

    KISS

--- src/App/index.js
+++ src/App/index.js
@@ -1,29 +1,15 @@
-import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import View from './view';
 import { counterReset, counterAdd } from '../redux/actions';
 
-class App extends Component {
-  static propTypes = {
-    ...View.propTypes,
-  };
-  handleAdd = (e) => {
-    this.props.handleAdd(e.target.value);
-  };
-  render() {
-    const { current, handleReset } = this.props;
-    return <View current={current} handleReset={handleReset} handleAdd={this.handleAdd} />;
-  }
-}
-
 const mapStateToProps = state => ({
   current: state.counter.current,
 });
 
-const mapDispatchToProps = {
-  handleReset: counterReset,
-  handleAdd: counterAdd,
-};
+const mapDispatchToProps = dispatch => ({
+  handleReset: () => dispatch(counterReset()),
+  handleAdd: e => dispatch(counterAdd(e.target.value)),
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(View);
